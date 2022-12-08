ba62c8bac973c397ac86041462c0e5dcf642c835
Nidhulsha <108129412+Nidhulsha@users.noreply.github.com>
2022-12-08 14:18:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+str="Hello everyone"
+print(str)
+print(str[0:5])
+print(str[6:])
+print(str[0:6] + "all")
+
+list1=[1,2,3,4]
+list2=['one','two','three','four']
+print(list1)
+print(list1 + list2)
+
+str1 = "Welcome"
+print(str1)
+del str1
